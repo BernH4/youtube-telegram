83de5069ccaf510d93dea060a9bf1410082a2f23
BernH4 <bernhard.steidl14@gmail.com>
2020-09-20 11:41:32 +0200

    Able to send audio

---
+++ bot.rb
@@ -0,0 +1,14 @@
+require 'telegram/bot'
+
+token = '1077149743:AAF8h-m2cO80DDmpmnQyUeDfsJOPZsoxWxY'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Was geht ab, #{message.from.first_name}?")
+    when '/mp3'
+      bot.api.send_Audio(chat_id: message.chat.id, audio: Faraday::UploadIO.new('/home/pi/test.mp3'))
+    end
+  end
+end
