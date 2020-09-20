00d552b7a8799eca94b523cfb284b830fe0c45a9
BernH4 <bernhard.steidl14@gmail.com>
2020-09-20 11:49:16 +0200

    Fixed sending mp3

--- bot.rb
+++ bot.rb
@@ -8,7 +8,7 @@ Telegram::Bot::Client.run(token) do |bot|
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Was geht ab, #{message.from.first_name}?")
     when '/mp3'
-      bot.api.send_Audio(chat_id: message.chat.id, audio: Faraday::UploadIO.new('/home/pi/test.mp3'))
+      bot.api.send_Audio(chat_id: message.chat.id, audio: Faraday::UploadIO.new('/home/pi/test.mp3', 'audio/mp3'))
     end
   end
 end
