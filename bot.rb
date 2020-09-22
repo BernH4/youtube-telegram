07914c3572c220303864b20d63159f6330c4e60a
BernH4 <bernhard.steidl14@gmail.com>
2020-09-22 15:27:52 +0200

    created /start message

--- bot.rb
+++ bot.rb
@@ -10,10 +10,12 @@ Telegram::Bot::Client.run(token) do |bot|
     puts "\nUser input: #{message.text}"
     timer_start = Time.now
     case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Paste YouTube link to convert to mp3")
     when 'Test'
+      puts "Testing.."
       bot.api.send_message(chat_id: message.chat.id, text: "Running and working!")
     when youtube_regex
-      puts "YouTube link recognized, fetching name"
       bot.api.send_message(chat_id: message.chat.id, text: "YouTube link recognized, fetching name")
       filename = Open3.capture3("youtube-dl", "--get-filename", "-o", '%(title)s.mp3', message.text).first.chomp.tr("\"", "")
   
@@ -22,7 +24,6 @@ Telegram::Bot::Client.run(token) do |bot|
       stdout_dl, stderr_dl, status_dl = Open3.capture3("youtube-dl", "-x","-o", "/home/pi/Downloads/music/%(title)s.%(ext)s", "--add-metadata", "--xattrs", "--embed-thumbnail", "--audio-format", "mp3", "--audio-quality", "0", message.text)
       bot.api.send_Audio(chat_id: message.chat.id, audio: Faraday::UploadIO.new("/home/pi/Downloads/music/#{filename}", 'audio/mp3'))
       bot.api.send_message(chat_id: message.chat.id, text: "Finished in #{(Time.now - timer_start).truncate} seconds!")
-      puts "Finished!"
     end
   end
 end
